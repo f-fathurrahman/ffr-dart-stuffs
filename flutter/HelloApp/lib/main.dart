5ed76183447140c12b95d8f15838a59f690d69d8
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2020-09-24 10:11:18 +0700

    Test CounterApp v02

--- flutter/HelloApp/lib/main.dart
+++ flutter/HelloApp/lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import "CounterApp02.dart";
 
-import 'TestIconWidget.dart';
+//import 'TestIconWidget.dart';
 
 //void main() => runApp(RootWidget());
 //void main() => runApp(FancyHelloWidget());
-void main() => runApp(TestIconWidget());
+//void main() => runApp(TestIconWidget());
+
+void main() {
+  runApp(CounterApp02());
+}
 
