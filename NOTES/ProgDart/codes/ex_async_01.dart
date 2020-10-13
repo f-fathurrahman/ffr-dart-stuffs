971354b156d631fb193a1240ff143efcb38e24d7
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2020-10-13 12:39:06 +0700

    Test async

---
+++ NOTES/ProgDart/codes/ex_async_01.dart
@@ -0,0 +1,29 @@
+// From: http://jpryan.me/dartbyexample/examples/async_await/
+
+import 'dart:async';
+
+const Duration delay = const Duration(milliseconds: 200);
+
+// This function does not use async/await, just the standard Future API
+Future loadLastName(String prefix) {
+  return Future.delayed(delay).then( (_) {
+    return prefix + 'son';
+  });
+}
+
+// Marking a function with 'async' will return a future
+// that completes with the returned value.
+// This function is equivalent to [loadLastName]
+Future loadLastName2(String prefix) async {
+  await new Future.delayed(delay);
+  return prefix + 'son';
+}
+
+
+void main() async {
+  var thorsLastName = await loadLastName('Odin');
+  var lokisLastName = await loadLastName('Laufey');
+
+  print('Thor $thorsLastName');
+  print('Loki $lokisLastName');
+}
