b7ab46ada45960df80be1372be1c268c8dbd5d5d
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2020-11-05 22:36:45 +0700

    Test formatting text

--- flutter/TestFlutterTeX/lib/main.dart
+++ flutter/TestFlutterTeX/lib/main.dart
@@ -32,28 +32,34 @@ class MyHomePage extends StatelessWidget {
 class MyTeXView extends StatelessWidget {
   Widget build(BuildContext context) {
     return TeXView(
-      child: TeXViewColumn(children: [
-        TeXViewInkWell(
-          id: "id_0",
-          child: TeXViewColumn(children: [
-            TeXViewDocument(r"""<h2>Flutter \( \rm\\TeX \)</h2>""",
-                style: TeXViewStyle(textAlign: TeXViewTextAlign.Center)),
-            TeXViewContainer(
-              child: TeXViewImage.network(
-                  'https://raw.githubusercontent.com/shah-xad/flutter_tex/master/example/assets/flutter_tex_banner.png'),
-              style: TeXViewStyle(
-                margin: TeXViewMargin.all(10),
-                borderRadius: TeXViewBorderRadius.all(20),
-              ),
-            ),
-            TeXViewDocument(r"""<p>                                
+      child: TeXViewColumn(
+        children: [
+          TeXViewInkWell(
+            id: "id_0",
+            child: TeXViewColumn(
+              children: [
+                TeXViewDocument(r"""<h2>Flutter \( \rm\\TeX \)</h2>""",
+                    style: TeXViewStyle(textAlign: TeXViewTextAlign.Center)),
+                TeXViewContainer(
+                  child: TeXViewImage.network(
+                      'https://raw.githubusercontent.com/shah-xad/flutter_tex/master/example/assets/flutter_tex_banner.png'),
+                  style: TeXViewStyle(
+                    margin: TeXViewMargin.all(10),
+                    borderRadius: TeXViewBorderRadius.all(20),
+                  ),
+                ),
+                TeXViewDocument(
+                  r"""<p>                                
                        When \(a \ne 0 \), there are two solutions to \(ax^2 + bx + c = 0\) and they are
                        $$x_{1,2} = {-b \pm \sqrt{b^2-4ac} \over 2a}.$$</p>""",
-                style: TeXViewStyle.fromCSS(
-                    'font-size: 20pt; padding: 15px; color: white; background: green'))
-          ]),
-        )
-      ]),
+                  style: TeXViewStyle.fromCSS(
+                      'font-size: 20pt; padding: 15px; color: white; background: green'),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
       style: TeXViewStyle(
         elevation: 10,
         borderRadius: TeXViewBorderRadius.all(25),
