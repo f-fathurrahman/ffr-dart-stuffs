297a72bafdf51a89ad4f452d510ba79a4b354062
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2020-11-13 08:54:59 +0700

    Ignore punya_mahasiswa

--- NOTES/ProgFlutter/counter_app_01/lib/main.dart
+++ NOTES/ProgFlutter/counter_app_01/lib/main.dart
@@ -1,5 +1,5 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:counter_app_01/fancy_button.dart';
 
 void main() => runApp(CounterApp());
 
@@ -9,56 +9,86 @@ class CounterApp extends StatelessWidget {
     return MaterialApp(
       title: 'Counter App by Kelompok XXX',
       theme: ThemeData(primarySwatch: Colors.green),
-      home: CounterAppHome(title: 'Counter App Home'),
+      home: CounterAppHome(),
     );
   }
 }
 
-class CounterAppHome extends StatelessWidget {
-  final String title;
-  CounterAppHome({Key key, this.title}) : super(key: key);
+class CounterAppHome extends StatefulWidget {
+  @override
+  _CounterAppHomeState createState() => _CounterAppHomeState();
+}
+
+class _CounterAppHomeState extends State<CounterAppHome> {
+
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState( () {
+      _counter = _counter + 1;
+      print('Increment button is pressed: $_counter');
+    } );
+  }
+
+  void _decrementCounter() {
+    setState( () {
+      _counter = _counter - 1;
+      print('Decrement button is pressed: $_counter');
+    } );
+  }
+
+  void _resetCounter() {
+    setState( () {
+      _counter = 0;
+      print('Reset button is pressed');
+    } );
+  }
 
   @override
   Widget build(BuildContext context) {
-    final incrementButton = FancyButton(
-      child: Text(
-        'Increment',
-        style: TextStyle(color: Colors.white),
+    
+    final incrementButton = Container(
+      child: RaisedButton(
+        child: Text('Increment', style: TextStyle(color: Colors.white)),
+        color: Colors.red,
+        onPressed: _incrementCounter,
       ),
     );
 
-    final decrementButton = FancyButton(
-      child: Text(
-        "Decrement",
-        style: TextStyle(color: Colors.white),
+    final decrementButton = Container(
+      child: RaisedButton(
+        child: Text('Decrement', style: TextStyle(color: Colors.white)),
+        color: Colors.blue,
+        onPressed: _decrementCounter,
       ),
     );
 
     var _buttons = <Widget>[incrementButton, decrementButton];
+    var _flutterLogo = Container(
+      margin: EdgeInsets.only(bottom: 5.0),
+      padding: EdgeInsets.all(8.0),
+      decoration: BoxDecoration(
+        color: Colors.blue.withOpacity(0.25),
+        borderRadius: BorderRadius.circular(4.0),
+      ),
+      child: Image.asset(
+        'images/flutter_logo_1080.png',
+        width: 100,
+      ),
+    );
 
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text('Counter App Home'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Container(
-              margin: EdgeInsets.only(bottom: 50.0),
-              padding: EdgeInsets.all(8.0),
-              decoration: BoxDecoration(
-                color: Colors.blue.withOpacity(0.25),
-                borderRadius: BorderRadius.circular(4.0),
-              ),
-              child: Image.asset(
-                'images/flutter_logo_1080.png',
-                width: 100,
-              ),
-            ),
+            _flutterLogo,
             Text('You have pushed the button this many times: '),
             Text(
-              '0',
+              _counter.toString(),
               style: Theme.of(context).textTheme.headline4,
             ),
             Row(
@@ -71,6 +101,7 @@ class CounterAppHome extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         tooltip: 'Reset',
         child: new Icon(Icons.refresh),
+        onPressed: _resetCounter,
       ),
     );
 
